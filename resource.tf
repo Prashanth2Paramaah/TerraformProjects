a241528a23bd938d944632a86b8f6904c9b73bf5
Prashanth2Paramaah <122254628+Prashanth2Paramaah@users.noreply.github.com>
2023-11-10 14:36:39 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -9,19 +9,22 @@ resource "aws_instance" "project-test" {
   }
 provisioner "remote-exec" {
   inline = [
-    "sudo yum update -y",
+    # Add Docker's official GPG key:
+    "sudo apt-get update",
+    "sudo apt-get install ca-certificates curl gnupg",
+    "sudo install -m 0755 -d /etc/apt/keyrings",
+    "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
+    "sudo chmod a+r /etc/apt/keyrings/docker.gpg",
     "touch file3",
-    "sudo yum install git -y",
-    "echo 'Git Installation Is Successful'",
-    "sudo git --version",
-    "sudo yum install -y yum-utils",
-    "sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo",
-    "sudo yum install docker-ce docker-ce-cli containerd.io -y",
-    "sudo systemctl start docker",
-    "sudo systemctl enable docker",
     "echo 'Docker Installation Is Successful'",
     "sudo docker --version",
     "touch file4"
+     "echo \
+         "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+         "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+          sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt-get update"
+
     ]
 }
 connection {
