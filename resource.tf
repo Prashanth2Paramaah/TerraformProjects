6086259f609ad11d8e244abf0265e241f25eea18
Prashanth2Paramaah <122254628+Prashanth2Paramaah@users.noreply.github.com>
2023-12-11 17:53:46 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -11,10 +11,10 @@ provisioner "remote-exec" {
   inline = [
     "sudo touch file1",
     "sudo apt update",
-    "sudo apt install openjdk-11-jdk -y",
+    "sudo apt install fontconfig openjdk-17-jre -y",
     "sudo java -version",
-    "sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
-    "sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
+    "sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \ https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key,
+    "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \ https://pkg.jenkins.io/debian-stable binary/ | sudo tee \ /etc/apt/sources.list.d/jenkins.list > /dev/null",
     "sudo apt-get update",
     "sudo apt-get install jenkins -y",
     "sudo systemctl status jenkins",
