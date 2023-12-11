ca0248636409c8216e361a85908ccbe4e53436a3
Prashanth2Paramaah <122254628+Prashanth2Paramaah@users.noreply.github.com>
2023-12-11 18:55:40 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -11,17 +11,12 @@ provisioner "remote-exec" {
   inline = [
     "sudo touch file1",
     "sudo apt update",
-    "sudo apt install default-jre -y",
-    "sudo java -version",
-    "sudo wget -O /usr/share/keyrings/jenkins-keyring.asc   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key",
-    "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null",
-    "sudo apt-get update",
-    "sudo apt-get install jenkins -y",
-    "sudo systemctl status jenkins",
-    "sudo systemctl enable jenkins",
-    "sudo systemctl start jenkins",
-    "echo jenkins server is started",
-    "sudo systemctl status jenkins"
+    "sudo apt install apt-transport-https ca-certificates curl software-properties-common -y",
+    "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+    "echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"
+    "sudo apt update"
+    "sudo apt install docker-ce -y"
+    "sudo systemctl status docker"
     ]
 }
 connection {
